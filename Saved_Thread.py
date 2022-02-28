3d7479a936e28b2081915bea3780736b97274d77
Franco0700 <70175704+Franco0700@users.noreply.github.com>
2022-02-28 03:42:52 -0300

    new logging system and some bug fixes

--- Saved_Thread.py
+++ Saved_Thread.py
@@ -4,4 +4,10 @@ class SavedThread():
         self.owner = own
         self.time = ti
         self.mustContinue = True
-        
+    
+    def __str__(self):
+        return ( 'id: ' + str(self.owner) + '\n' +
+                 'index: ' + str(self.index) + '\n' +
+                 'time: ' + str(self.time) + '\n' +
+                 'cont: ' + str(self.mustContinue) + '\n'
+                )
